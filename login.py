58ba9e9a910159014fa72a0d695ffd51e2c480ba
John Smith <xfgryujk@126.com>
2019-04-20 14:11:30 +0800

    自动恢复身份，不需要重新登录

--- login.py
+++ login.py
@@ -1,119 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-微博登录需要验证码时的临时解决方案
+微博登录需要验证码时的解决方案，要在GUI环境下运行
 """
 
-import base64
-import binascii
 import json
-import re
 from asyncio import get_event_loop
 from io import BytesIO
-from random import randint
-from urllib.parse import quote_plus
 
-import rsa
 from PIL import Image
-from aiohttp import ClientSession
+
+from weibo import WeiboApi
 
 
-class WeiboApi:
-    def __init__(self):
-        self._session = ClientSession()
-
-    async def close(self):
-        await self._session.close()
-
-    def save_cookie(self, path):
-        self._session.cookie_jar.save(path)
-
-    # https://github.com/xchaoinfo/fuck-login/blob/master/007%20weibo.com/weibo.com.py
-    async def login(self, username, password):
-        print('登录中')
-        su = self._get_su(username)
-        sever_data = await self._pre_login(su)
-        servertime = sever_data['servertime']
-        nonce = sever_data['nonce']
-        rsakv = sever_data['rsakv']
-        pubkey = sever_data['pubkey']
-        showpin = sever_data['showpin']
-        password_secret = self._get_password(password, servertime, nonce, pubkey)
-        door = '' if showpin == 0 else await self._input_verif_code(sever_data['pcid'])
-
-        async with self._session.post('http://login.sina.com.cn/sso/login.php', params={
-            'client': 'ssologin.js(v1.4.18)',
-        }, data={
-            'entry':      'weibo',
-            'gateway':    '1',
-            'from':       '',
-            'savestate':  '7',
-            'useticket':  '1',
-            'pagerefer':  'http://login.sina.com.cn/sso/logout.php?entry=miniblog'
-                          '&r=http%3A%2F%2Fweibo.com%2Flogout.php%3Fbackurl',
-            'vsnf':       '1',
-            'su':         su,
-            'service':    'miniblog',
-            'servertime': servertime,
-            'nonce':      nonce,
-            'pwencode':   'rsa2',
-            'rsakv':      rsakv,
-            'sp':         password_secret,
-            'sr':         '1366*768',
-            'encoding':   'UTF-8',
-            'prelt':      '115',
-            'url':        'http://weibo.com/ajaxlogin.php?framelogin=1&callback='
-                          'parent.sinaSSOController.feedBackUrlCallBack',
-            'returntype': 'META',
-            'door':       door
-        }) as r:
-            res = await r.text()
-        url = re.findall(r'location\.replace\([\'"](.*?)[\'"]\)', res)[0]
-
-        async with self._session.get(url) as r:
-            await r.read()
-
-    @staticmethod
-    def _get_su(username):
-        """URL编码再BASE64编码"""
-        return base64.b64encode(quote_plus(username).encode()).decode()
-
-    async def _pre_login(self, su):
-        async with self._session.get('http://login.sina.com.cn/sso/prelogin.php', params={
-            'entry':    'weibo',
-            'callback': 'sinaSSOController.preloginCallBack',
-            'su':       su,
-            'rsakt':    'mod',
-            'checkpin': '1',
-            'client':   'ssologin.js(v1.4.18)'
-        }) as r:
-            res = await r.text()
-        res = res[res.find('{'): res.rfind('}') + 1]
-        return json.loads(res)
-
-    @staticmethod
-    def _get_password(password, servertime, nonce, pubkey):
-        key = rsa.PublicKey(int(pubkey, 16), 65537)
-        res = rsa.encrypt(f'{servertime}\t{nonce}\n{password}'.encode(), key)
-        res = binascii.b2a_hex(res)
-        return res.decode()
-
-    async def _input_verif_code(self, pcid):
-        async with self._session.get('https://login.sina.com.cn/cgi/pin.php', params={
-            'r': randint(0, 100000000),
-            's': '0',
-            'p': pcid
-        }) as r:
-            img_data = await r.read()
+class WeiboApiGui(WeiboApi):
+    def _show_image(self, img_data):
         img = Image.open(BytesIO(img_data))
         img.show()
-        return input('输入验证码：')
 
 
 async def main():
     with open('config.json') as f:
         config = json.load(f)
-    weibo = WeiboApi()
+    weibo = WeiboApiGui()
     await weibo.login(config['weibo_username'], config['weibo_password'])
     weibo.save_cookie('weibo_cookie.pickle')
     await weibo.close()
--- weibo.py
+++ weibo.py
@@ -1,15 +1,38 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import base64
 import binascii
 import json
+import random
 import re
-import time
 from urllib.parse import quote_plus
 
 import rsa
 from aiohttp import ClientSession, ContentTypeError, ClientError
-from yarl import URL
+
+# 修复cookie expire星期不能匹配的BUG
+import http.cookies
+http.cookies.BaseCookie._BaseCookie__parse_string.__defaults__ = (
+    re.compile(r"""
+        \s*                            # Optional whitespace at start of cookie
+        (?P<key>                       # Start of group 'key'
+        [""" + http.cookies._LegalKeyChars + r"""]+?   # Any word of at least one letter
+        )                              # End of group 'key'
+        (                              # Optional group: there may not be a value.
+        \s*=\s*                          # Equal Sign
+        (?P<val>                         # Start of group 'val'
+        "(?:[^\\"]|\\.)*"                  # Any doublequoted string
+        |                                  # or
+        \w{3,},\s[\w\d\s-]{9,11}\s[\d:]{8}\sGMT  # Special case for "expires" attr
+        |                                  # or
+        [""" + http.cookies._LegalValueChars + r"""]*      # Any word or empty string
+        )                                # End of group 'val'
+        )?                             # End of optional value group
+        \s*                            # Any number of spaces.
+        (\s+|;|$)                      # Ending either at space, semicolon, or EOS.
+        """, re.ASCII | re.VERBOSE),   # re.ASCII may be removed if safe.
+)
 
 
 class WeiboApi:
@@ -26,63 +49,119 @@ class WeiboApi:
         self._session.cookie_jar.save(path)
 
     async def login_if_need(self, username, password):
-        async with self._session.get('https://weibo.com/') as r:
-            url = URL(r.url)
-        # 被重定向到访客系统
-        if url.host != 'weibo.com' or '/home' not in url.path:
+        if not await self.restore_session():
             await self.login(username, password)
 
-    # https://github.com/xchaoinfo/fuck-login/blob/master/007%20weibo.com/weibo.com.py
-    async def login(self, username, password):
-        print('登录中')
-        su = self._get_su(username)
-        sever_data = await self._pre_login(su)
-        servertime = sever_data['servertime']
-        nonce = sever_data['nonce']
-        rsakv = sever_data['rsakv']
-        pubkey = sever_data['pubkey']
-        showpin = sever_data['showpin']
-        password_secret = self._get_password(password, servertime, nonce, pubkey)
-        assert showpin == 0, '需要验证码'
+    async def restore_session(self):
+        async with self._session.get('https://weibo.com/') as r:
+            return await self.__handle_login_page(str(r.url), await r.text())
 
-        async with self._session.post('http://login.sina.com.cn/sso/login.php', params={
-            'client': 'ssologin.js(v1.4.18)',
-        }, data={
-            'entry':      'weibo',
-            'gateway':    '1',
-            'from':       '',
-            'savestate':  '7',
-            'useticket':  '1',
-            'pagerefer':  'http://login.sina.com.cn/sso/logout.php?entry=miniblog'
-                          '&r=http%3A%2F%2Fweibo.com%2Flogout.php%3Fbackurl',
-            'vsnf':       '1',
-            'su':         su,
-            'service':    'miniblog',
-            'servertime': servertime,
-            'nonce':      nonce,
-            'pwencode':   'rsa2',
-            'rsakv':      rsakv,
-            'sp':         password_secret,
-            'sr':         '1366*768',
-            'encoding':   'UTF-8',
-            'prelt':      '115',
-            'url':        'http://weibo.com/ajaxlogin.php?framelogin=1&callback='
-                          'parent.sinaSSOController.feedBackUrlCallBack',
-            'returntype': 'META'
-        }) as r:
-            res = await r.text()
-        url = re.findall(r'location\.replace\([\'"](.*?)[\'"]\)', res)[0]
+    async def __handle_login_page(self, url, res):
+        while True:
+            # 登录页
+            if url.startswith('https://login.sina.com.cn/sso/login.php'):
+                next_url = self.__get_next_url(res)
 
-        async with self._session.get(url) as r:
-            await r.read()
+            # 新浪访客系统，用来恢复cookie
+            elif url.startswith('https://passport.weibo.com/visitor/visitor'):
+                if 'a=enter' in url:
+                    next_url = 'https://passport.weibo.com/visitor/visitor?a=restore&cb=restore_back&from=weibo'
+                else:
+                    res_ = self.__get_jsonp_response(res)
+                    next_url = (
+                        f'https://login.sina.com.cn/sso/login.php?entry=sso&alt={res_["data"]["alt"]}'
+                        f'&returntype=META&gateway=1&savestate={res_["data"]["savestate"]}'
+                        f'&url=https%3A%2F%2Fweibo.com%2F%3Fdisplay%3D0%26retcode%3D6102'
+                    )
+
+            # 跨域登录广播
+            elif url.startswith('https://login.sina.com.cn/crossdomain2.php'):
+                async def cross_domain_callback(url_, i):
+                    async with self._session.get(url_, params={
+                        'callback': 'sinaSSOController.doCrossDomainCallBack',
+                        'scriptId': 'ssoscript' + str(i),
+                        'client': 'ssologin.js(v1.4.2)'
+                    }) as r_:
+                        await r_.read()
+
+                url_list = re.search(r'setCrossDomainUrlList\((.*?)\);', res)[1]
+                url_list = json.loads(url_list)['arrURL']
+                await asyncio.gather(*(
+                    cross_domain_callback(url, i) for i, url in enumerate(url_list)
+                ))
+
+                next_url = self.__get_next_url(res)
+
+            # 调用parent.sinaSSOController.feedBackUrlCallBack跳转到weibo.com/nguide/interest
+            elif url.startswith('https://weibo.com/ajaxlogin.php'):
+                res_ = self.__get_jsonp_response(res)
+                next_url = res_['redirect']
+
+            # 登录完毕
+            elif url.startswith('https://weibo.com'):
+                return '/home' in url
+
+            # 未知的地址
+            else:
+                print('未知的地址：' + url)
+                print(res)
+                return False
+
+            async with self._session.get(next_url, headers={
+                'Referer': url  # 访问visitor?a=restore必须带referer
+            }) as r:
+                url = str(r.url)
+                res = await r.text()
 
     @staticmethod
-    def _get_su(username):
-        """URL编码再BASE64编码"""
-        return base64.b64encode(quote_plus(username).encode()).decode()
+    def __get_next_url(html):
+        match = re.search(r'location\.replace\([\'"](.*?)[\'"]\)', html)
+        return match[1] if match is not None else None
+
+    @staticmethod
+    def __get_jsonp_response(js):
+        return json.loads(js[js.find('{'): js.rfind('}') + 1])
+
+    async def login(self, username, password):
+        print('登录中')
+        # URL编码再BASE64编码
+        su = base64.b64encode(quote_plus(username).encode()).decode()
+        data = await self._pre_login(su)
+
+        async with self._session.post('https://login.sina.com.cn/sso/login.php', params={
+            'client': 'ssologin.js(v1.4.19)',
+        }, data={
+            'entry':       'weibo',
+            'gateway':     '1',
+            'from':        '',
+            'savestate':   '7',
+            'qrcode_flag': 'false',
+            'useticket':   '1',
+            'pagerefer':   'https://login.sina.com.cn/crossdomain2.php?action=logout&'
+                           'r=https%3A%2F%2Fpassport.weibo.com%2Fwbsso%2Flogout%3Fr%3'
+                           'Dhttps%253A%252F%252Fweibo.com%26returntype%3D1',
+            'vsnf':        '1',
+            'su':          su,
+            'service':     'miniblog',
+            'servertime':  data['servertime'],
+            'nonce':       data['nonce'],
+            'pwencode':    'rsa2',
+            'rsakv':       data['rsakv'],
+            'sp':          self._get_secret_password(password, data['servertime'],
+                                                     data['nonce'], data['pubkey']),
+            'sr':          '1366*768',
+            'encoding':    'UTF-8',
+            'prelt':       '233',
+            'url':         'https://weibo.com/ajaxlogin.php?framelogin=1&callback='
+                           'parent.sinaSSOController.feedBackUrlCallBack',
+            'returntype':  'META',
+            'door':        '' if data['showpin'] == 0
+                           else await self._input_verif_code(data['pcid'])
+        }) as r:
+            return await self.__handle_login_page(str(r.url), await r.text())
 
     async def _pre_login(self, su):
-        async with self._session.get('http://login.sina.com.cn/sso/prelogin.php', params={
+        async with self._session.get('https://login.sina.com.cn/sso/prelogin.php', params={
             'entry':    'weibo',
             'callback': 'sinaSSOController.preloginCallBack',
             'su':       su,
@@ -90,12 +169,23 @@ class WeiboApi:
             'checkpin': '1',
             'client':   'ssologin.js(v1.4.18)'
         }) as r:
-            res = await r.text()
-        res = res[res.find('{'): res.rfind('}') + 1]
-        return json.loads(res)
+            return self.__get_jsonp_response(await r.text())
+
+    async def _input_verif_code(self, pcid):
+        async with self._session.get('https://login.sina.com.cn/cgi/pin.php', params={
+            'r': random.randint(0, 100000000),
+            's': '0',
+            'p': pcid
+        }) as r:
+            img_data = await r.read()
+        self._show_image(img_data)
+        return input('输入验证码：')
+
+    def _show_image(self, img_data):
+        raise NotImplementedError('需要验证码')
 
     @staticmethod
-    def _get_password(password, servertime, nonce, pubkey):
+    def _get_secret_password(password, servertime, nonce, pubkey):
         key = rsa.PublicKey(int(pubkey, 16), 65537)
         res = rsa.encrypt(f'{servertime}\t{nonce}\n{password}'.encode(), key)
         res = binascii.b2a_hex(res)
@@ -123,15 +213,15 @@ class WeiboApi:
                 }, allow_redirects=False) as r:
                     if 'Location' not in r.headers:
                         continue
-                    res = re.findall(r'&pid=(.*?)(&|$)', r.headers['Location'])
+                    res = re.search(r'&pid=(.*?)(&|$)', r.headers['Location'])
             except (ClientError, IOError, TimeoutError):
                 print('超时')
                 continue
-            if not res:
+            if res is None:
                 print(r.headers)
                 print(await r.text())
                 continue
-            return res[0][0]
+            return res[1]
         return None
 
     async def post_weibo(self, text, image_ids):
